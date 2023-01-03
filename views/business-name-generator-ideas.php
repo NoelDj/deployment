a8d339363bd1630077faef8273157df172e5e079
NoelDj <noel0045@stud.kea.dk>
2023-01-03 16:03:34 +0100

    removed comments

--- views/business-name-generator-ideas.php
+++ views/business-name-generator-ideas.php
@@ -46,12 +46,12 @@ require_once 'components/footer.php';
             const value = document.querySelector('#text-input').value
 
             if (value.length < 2) {
-                alert('Names is too short')
+                alert('Name is too short')
                 return
             }
 
             if (value.length > 20) {
-                alert('Names is too long')
+                alert('Name is too long')
                 return
             }
 
@@ -92,13 +92,13 @@ require_once 'components/footer.php';
         const p = new RegExp(a.split('').join('|'), 'g')
 
         return string.toString().toLowerCase()
-            .replace(/\s+/g, '-') // Replace spaces with -
-            .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-            .replace(/&/g, '-and-') // Replace & with 'and'
-            .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-            .replace(/\-\-+/g, '-') // Replace multiple - with single -
-            .replace(/^-+/, '') // Trim - from start of text
-            .replace(/-+$/, '') // Trim - from end of text
+            .replace(/\s+/g, '-')
+            .replace(p, c => b.charAt(a.indexOf(c)))
+            .replace(/&/g, '-and-')
+            .replace(/[^\w\-]+/g, '')
+            .replace(/\-\-+/g, '-')
+            .replace(/^-+/, '')
+            .replace(/-+$/, '')
     }
 </script>
 
